b401e56cdc8e956347ba3ec3114e863be9f387a2
khaihoan010 <46442140+khaihoan010@users.noreply.github.com>
2019-06-04 19:12:30 +0700

    crud ajax, theme app success

---
+++ ClothShopK.Web/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using ClothShopK.Entities;
+using ClothShopK.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothShopK.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public ActionResult ProductTable(string search)
+        {
+            var products = productsService.GetProducts();
+
+            if (string.IsNullOrEmpty(search)==false)
+            {
+                products = products.Where(p => p.Name!=null&&p.Name.ToLower().Contains(search.ToLower())).ToList();
+
+            }
+
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var product = productsService.GetProduct(ID);
+
+            return PartialView(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            productsService.UpdateProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int ID)
+        {
+            productsService.DeleteProduct(ID);
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
